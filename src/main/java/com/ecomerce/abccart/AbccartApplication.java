5796f81ed289bef8a63090d88889536e861feddf
satadu555 <satadru1991@gmail.com>
2024-07-21 23:10:07 +0530

    Amik Project

---
+++ src/main/java/com/ecomerce/abccart/AbccartApplication.java
@@ -0,0 +1,13 @@
+package com.ecomerce.abccart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbccartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbccartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomerce/abccart/AbccartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.abccart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbccartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
